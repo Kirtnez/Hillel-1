c62c20c6ab572ea04298a032e39cace54edfe8b9
Kirtnez <mario220684@gmail.com>
2025-10-10 18:14:40 +0200

    Home work 3.1

--- Lesson 3.py
+++ Lesson 3.py
@@ -8,9 +8,12 @@ if user == "+":
 elif user == "-":
     print(user_n1 - user_n2)
 
-elif user == "/":
-    print(user_n1 / user_n2)
-
 elif user == "*":
     print(user_n1 * user_n2)
 
+elif user == "/":
+    if user_n1 == 0:
+        print ("Ошибка")
+        # elif user_n2 == 0: Почему так не будет работать елси он в блоке if ??
+else:
+    print(user_n1 / user_n2)
