34c41f951459e2141eec2bb2ed4f7290f9b9ca11
Kirtnez <mario220684@gmail.com>
2025-11-01 21:10:15 +0100

    Home work 6.1

--- 7.1.py
+++ 7.1.py
@@ -2,4 +2,4 @@ def name1 (name, age):
     return f"Hi. My name is {name} and I`m {age} years old."
 assert name1("Alex", 32) == "Hi. My name is Alex and I`m 32 years old."
 assert name1("Frank", 52) == "Hi. My name is Frank and I`m 52 years old."
-print ("ok")
+print ("Ok")
